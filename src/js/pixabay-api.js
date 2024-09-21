ab79685a21322a086168a63cdd15a03d0324a16d
missmurs <131698859+missmurs@users.noreply.github.com>
2024-09-21 13:20:34 +0300

    Photo Search App

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const KEY = '41310960-a447b99d1b40cb87b689e5a50';
+const URL = 'https://pixabay.com/api/';
+
+export class NewsApiServer {
+  constructor() {
+    this.searchQuery = '';
+
+    this.per_page = 40;
+    this.totalImgs = 0;
+  }
+  async fetchImages() {
+    try {
+      const response = await axios.get(
+        `${URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.per_page}&page=${this.page}`
+      );
+      this.incrementPage();
+
+      console.log('p', this.page);
+      console.log('response.data', response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
